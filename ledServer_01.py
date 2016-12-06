afbedc4619f836df7377ba3b963a1e7d68665fa1
user <user@gmail.com>
2016-12-06 14:52:02 +0800

    init commit

---
+++ ledServer_01.py
@@ -0,0 +1,62 @@
+def runLedServer():
+	import network
+	ap_if = network.WLAN(network.AP_IF)
+	ap_if.config(essid="iLight", authmode=network.AUTH_WPA_WPA2_PSK, password="12345678")
+	import socket 
+	import machine
+	#HTML to send to browsers
+	html = """<!DOCTYPE html>
+	<html>
+	<head> <title>iLight</title> </head>
+	<form>
+	LED0: 
+	<button name="LED" value="ON0" type="submit">LED ON</button>
+	<button name="LED" value="OFF0" type="submit">LED OFF</button><br><br>
+	LED2: 
+	<button name="LED" value="ON2" type="submit">LED ON</button>
+	<button name="LED" value="OFF2" type="submit">LED OFF</button>
+	</form>
+	</html>
+	"""
+	#Setup PINS
+	LED0 = machine.Pin(0, machine.Pin.OUT)
+	LED2 = machine.Pin(2, machine.Pin.OUT)
+	#Setup Socket WebServer
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.bind(('', 80))
+	s.listen(5)
+	while True:
+		try:
+			conn, addr = s.accept()
+			print("Got a connection from %s\n\n" % str(addr))
+			request = conn.recv(1024)
+			print("Content = %s\n\n" % str(request))
+			request = str(request)
+			LEDON0 = request.find('/?LED=ON0')
+			LEDOFF0 = request.find('/?LED=OFF0')
+			LEDON2 = request.find('/?LED=ON2')
+			LEDOFF2 = request.find('/?LED=OFF2')
+			#print("Data: " + str(LEDON0))
+			#print("Data2: " + str(LEDOFF0))
+			if LEDON0 == 6:
+				print('TURN LED0 ON')
+				LED0.low()
+			if LEDOFF0 == 6:
+				print('TURN LED0 OFF')
+				LED0.high()
+			if LEDON2 == 6:
+				print('TURN LED2 ON')
+				LED2.low()
+			if LEDOFF2 == 6:
+				print('TURN LED2 OFF')
+				LED2.high()
+			print('\n\n\n')
+			response = html
+			conn.send(response)
+			conn.close()
+		except KeyboardInterrupt:
+			print("Stop server")
+			sta_if = network.WLAN(network.STA_IF)
+			sta_if.active(True)
+			sta_if.connect("YY WIFI", "mary1234")
+			break
