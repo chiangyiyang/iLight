582fa3804055bb5cf068e90676e5eec00123f79d
user <user@gmail.com>
2016-12-08 15:56:47 +0800

    iLight Version 0.1 This is a simple web ap for control RGB led via GET.

--- iLight.py
+++ iLight.py
@@ -6,20 +6,34 @@ import sys
 class iLightServer:
 	ssid = ''
 	pw = ''
-
+	ap_on = '1'
+	sta_on = '1'
+	
 	def updateWifi(self):
 		print('\nUpdate Wifi\n')
 		if sys.platform == 'esp8266':
 			import network
-			sta_if = network.WLAN(network.STA_IF)
-			sta_if.active(True)
-			sta_if.connect(self.ssid, self.pw)
-			sleep(15)
-			if not sta_if.isconnected():
-				print('Link to AP(%s) failed!!' % self.ssid)
-				ap_if = network.WLAN(network.AP_IF)
+			ap_if = network.WLAN(network.AP_IF)
+			if self.ap_on == '1':
+				ap_if.active(True)
 				ap_if.config(essid="iLight", authmode=network.AUTH_WPA_WPA2_PSK, password="12345678")
-
+				print('\nEnable AP MODE\n')
+			else:
+				print('\nDisable AP MODE\n')
+				ap_if.active(False)
+			
+			sta_if = network.WLAN(network.STA_IF)
+			if self.sta_on == '1':
+				print('\nEnable Station MODE\n')
+				sta_if.active(True)
+				sta_if.connect(self.ssid, self.pw)
+				sleep(10)
+				if not sta_if.isconnected():
+					print('Link to AP(%s) failed!!' % self.ssid)
+			else:
+				print('\nDisable Station MODE\n')
+				sta_if.active(False)
+	
 	def loadSetting(self):
 		print('Load Settings')
 		try:
@@ -27,18 +41,20 @@ class iLightServer:
 				kv = dict(s.split('=') for s in f.read().split('&'))
 				self.ssid = kv['ssid']
 				self.pw = kv['pw']
+				self.ap_on = kv['ap_on']
+				self.sta_on = kv['sta_on']
 				f.close()
 				self.updateWifi()
-
+		
 		except  Exception as ex:
 			print("Error occurred in loadSetting:\n%s\n\n" % ex)
-
+	
 	def saveSetting(self):
 		print('Save Settings')
 		with open("settings.ini", "w") as f:
-			f.write("ssid=%s&pw=%s" % (self.ssid, self.pw))
+			f.write("ssid=%s&pw=%s&ap_on=%s&sta_on=%s" % (self.ssid, self.pw, self.ap_on, self.sta_on))
 			f.close()
-
+	
 	def getPage(self, pgType):
 		if pgType == 'setup':
 			return ("""
@@ -54,11 +70,15 @@ class iLightServer:
 	Link: <a href="/icontrol/">Control</a><hr>
 	SSID:<input name="ssid" type="text" value="{0}"><br><br>
 	PASSWORD:<input  name="pw" type="password" value=""><br><br>
+	AP MODE:<input  name="ap_on" type="checkbox" value="1" {1}><br><br>
+	Station MODE:<input  name="sta_on" type="checkbox" value="1" {2}><br><br>
 	<button type="submit">OK</button>
 	<button type="reset">Reset</button>
 	</form>
 	</body>
-	</html>""".format(self.ssid))
+	</html>""".format(self.ssid,
+			          ('checked' if self.ap_on == '1' else ''),
+			          ('checked' if self.sta_on == '1' else '')))
 		if pgType == 'control':
 			return """<!DOCTYPE html>
 	<html>
@@ -73,25 +93,28 @@ class iLightServer:
 	</form>
 	</html>
 	"""
-
+	
 	def setup(self):
+		if self.isResetDown():
+			self.ap_on = '1'
 		if sys.platform == 'esp8266':
 			from machine import Pin, PWM
-			setButton = Pin(14, Pin.IN, Pin.PULL_UP)
-			doSet = setButton.value()
 			self.LedR = PWM(Pin(5), freq=500, duty=0)  # D1
 			self.LedG = PWM(Pin(4), freq=500, duty=0)  # D2
 			self.LedB = PWM(Pin(0), freq=500, duty=0)  # D3
 		else:
 			pass
-
+	
 	def isResetDown(self):
-		# return True
-		return False
-
+		if sys.platform == 'esp8266':
+			from machine import Pin
+			return Pin(14, Pin.IN, Pin.PULL_UP).value() == 0
+		else:
+			return False
+	
 	def isWifiSetup(self):
 		return True
-
+	
 	def run(self):
 		self.setup()  # set GPIO...
 		self.loadSetting()
@@ -114,23 +137,29 @@ class iLightServer:
 				print("Accept Connection from '%s'" % str(addr))
 				request = str(conn.recv(1024))
 				print("Content:\n[%s]\n\n\n" % (request))
-				pos = request.find('GET /favicon.ico HTTP/1.1')
-				if pos > -1:
-					# conn.send(self.pages[pgType])
-					conn.close()
-					continue
+				# pos = request.find('GET /favicon.ico HTTP/1.1')
+				# if pos == 2:
+				# 	conn.close()
+				# 	continue
 				pSt = request.find('/isetup/?ssid=')
 				if pSt == 6:
 					# setup page back   ex. /isetup/?ssid=myAP&pw=12345678 HTTP
 					print("Update settings")
 					pEnd = request.find(' HTTP/1.1')
-					kv = dict(s.split('=') for s in request[pSt + 9:pEnd].split('&'))
-					self.ssid = kv['ssid']
-					self.pw = kv['pw']
-					print("ESSID: '%s'  PW: '%s'" % (self.ssid, self.pw))
-					self.updateWifi()
-					self.saveSetting()
-
+					try:
+						kv = dict(s.split('=') for s in request[pSt + 9:pEnd].split('&'))
+						self.ssid = kv['ssid']
+						self.pw = kv['pw']
+						self.sta_on = (kv['sta_on'] if 'sta_on' in kv.keys() else '')
+						self.ap_on = (kv['ap_on'] if 'ap_on' in kv.keys() else '')
+						self.ap_on = ('1' if self.sta_on == '' else self.ap_on)
+						print("ssid: '%s'  pw: '%s'  ap_on: '%s'  sta_on: '%s'" % (
+							self.ssid, self.pw, self.ap_on, self.sta_on))
+						self.updateWifi()
+						self.saveSetting()
+					except:
+						pass
+				
 				pSt = request.find('/?RGB=')
 				if pSt > -1:
 					# control page post back
@@ -151,9 +180,13 @@ class iLightServer:
 				if request.find('GET /icontrol/') > -1:
 					pgType = 'control'
 				# print('\n\nResponse:\n%s\n\n\n' % (self.pages(pgType)))
-				conn.send(self.getPage(pgType).encode())
-				conn.close()
-				print('Connection is closed!!\n\n\n\n')
+				try:
+					conn.send(self.getPage(pgType).encode())
+					conn.close()
+					print('Connection is closed!!\n\n\n\n')
+				except OSError:
+					print('Error occurred when sending response!!\n\n\n\n')
+			
 			except KeyboardInterrupt:
 				# Save status
 				print("Save control status")
